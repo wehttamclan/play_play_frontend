0d833769112cf54b045f765edd8eba5bd4bd2046
bghalami <benjaminghalami@gmail.com>
2018-12-08 15:06:37 -0700

    Initial commit, set up all files and dependencies, set up sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,53 @@
+// const path = require('path');
+//
+// module.exports = {
+//   entry: {
+//     main: "./lib/index.js",
+//     test: "mocha!./test/index.js"
+//   },
+//   output: {
+//     path: __dirname,
+//     filename: "[name].bundle.js"
+//   },
+//   module: {
+    // loaders: [
+    //   { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+    //   { test: /\.css$/, loader: "style!css" },
+    //   { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
+    // ]
+//   },
+//   resolve: {
+//     extensions: ['', '.js', '.json', '.css', '.scss']
+//   }
+// };
+
 const path = require('path');
 
 module.exports = {
+  mode: "development",
   entry: {
     main: "./lib/index.js",
-    test: "mocha!./test/index.js"
+    test: "mocha-loader!./test/index.js"
   },
   output: {
-    path: __dirname,
-    filename: "[name].bundle.js"
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist')
   },
+
   module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.css$/, loader: "style!css" },
-      { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
-    ]
+    rules: [{
+      test: /\.js$/, // include .js files
+      enforce: "pre", // preload the jshint loader
+      exclude: /node_modules/, // exclude any and all files in the node_modules folder
+      use: [{
+        loader: "jshint-loader",
+        // more options in the optional jshint object
+        options: {  // ⬅ formally jshint property
+          camelcase: true,
+          emitErrors: false,
+          failOnHint: false
+        }
+      }]
+    }]
   },
-  resolve: {
-    extensions: ['', '.js', '.json', '.css', '.scss']
-  }
 };
